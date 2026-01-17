92a362c9ea6277b1c4179b291298ded9d0b68683
Kenan Isakovic <kenaniskvc@gmx.net>
2026-01-17 13:44:19 +0100

    #main Implemented base code for VideoController

---
+++ FormFlow.Backend/Controllers/VideoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormFlow.Backend.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class VideoController : ControllerBase {
+
+    [HttpGet]
+    public IActionResult Get(string userId, CancellationToken ct) => Ok();
+
+    [HttpPost("upload-video")]
+    public IActionResult Upload(IFormFile videoFile, CancellationToken ct) => Ok();
+
+}
