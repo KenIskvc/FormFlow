14fb52dba6c763ce2003bfa0f70d0f094ce54105
Kenan Isakovic <kenaniskvc@gmx.net>
2026-01-17 15:05:26 +0100

    #main fixed database user type, defined dbcontext, fixed model relations

--- FormFlow.Backend/Models/Video.cs
+++ FormFlow.Backend/Models/Video.cs
@@ -4,6 +4,7 @@ public class Video {
     public int Id { get; private set; }
     public required string FileName { get; set; }
     public required byte[] FileData { get; set; }
-    public required string? FormFlowUserId { get; set; }
-    public ICollection<Analysis> Analyses { get; set; } = [];
+    public string UserId { get; set; } = default!;
+    public virtual FormFlowUser User { get; set; } = default!;
+    public virtual ICollection<Analysis> Analyses { get; set; } = [];
 }
