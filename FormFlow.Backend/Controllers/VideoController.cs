037b0d473a39fc3f4e53a69b7e8083f564b69270
Kenan Isakovic <kenaniskvc@gmx.net>
2026-01-17 15:09:23 +0100

    #main added data fetch example

--- FormFlow.Backend/Controllers/VideoController.cs
+++ FormFlow.Backend/Controllers/VideoController.cs
@@ -1,4 +1,5 @@
-﻿using FormFlow.Backend.DTOs;
+﻿using FormFlow.Backend.Data;
+using FormFlow.Backend.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,10 +8,19 @@ namespace FormFlow.Backend.Controllers;
 [ApiController]
 public class VideoController : ControllerBase {
 
+    private readonly FormFlowDbContext _context;
+
+    public VideoController(FormFlowDbContext context) => _context = context;
+
     // To-Do: Check if user id or name, is it a query param or body form? 
     [HttpGet]
     [Authorize]
-    public IActionResult Get(string userId, CancellationToken ct) => Ok();
+    public IActionResult Get(string userId, CancellationToken ct) =>
+        //this all should be made from a service or repository
+        //and is an example of how to fetch data from the database
+        //var videos = await _context.Videos.Where(x => x.UserId == userId).ToListAsync();
+        //return Ok(videos);
+        Ok();
 
     // To-Do: Read and process video data (uses AI library) and upload if userName is present (binary data []) 
     [HttpPost("upload-video")]
