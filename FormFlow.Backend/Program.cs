654b610cf175d5fd9df5b00d251fce8d9193f5e9
Kenan Isakovic <kenaniskvc@gmx.net>
2026-01-18 15:02:22 +0100

    #13 created global http client for pose detection client

--- FormFlow.Backend/Program.cs
+++ FormFlow.Backend/Program.cs
@@ -15,6 +15,15 @@ builder.Services.AddIdentityCore<FormFlowUser>()
        .AddEntityFrameworkStores<FormFlowDbContext>()
        .AddApiEndpoints();
 
+builder.Services.AddHttpClient("PoseDetectionClient", client => {
+    client.BaseAddress = new Uri("http://10.123.177.51:8000");
+    client.Timeout = TimeSpan.FromSeconds(60);
+});
+
+builder.Services.AddTransient(sp =>
+    sp.GetRequiredService<IHttpClientFactory>()
+      .CreateClient("PoseDetectionClient"));
+
 builder.Services.Configure<IdentityOptions>(options => {
     options.SignIn.RequireConfirmedAccount = false;
     options.SignIn.RequireConfirmedEmail = false;
